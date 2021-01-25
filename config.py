774283237a9113d9758da6e23479b782d57508aa
dmintz7 <dmintz37@gmail.com>
2021-01-24 23:07:24 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,16 @@
+log_level="INFO"
+log_folder= "/mnt/Server/Scripts/Logs"
+backup_path="/mnt/Server/Backups"
+recycle_bin_path = "/mnt/Server/Recycle Bin"
+
+paths = 'Bitwarden;Docker;Plex;SQL Database'
+backup_days = 7
+minium_files = 3
+
+bitwarden_health_checks_url="f1a8d5a4-dd72-41d8-a694-2eccb2cfcc24"
+docker_health_checks_url="94bc6c12-cab7-4a46-abb2-80352680b477"
+
+mysql_host="rachael.skynet.com"
+mysql_user="service"
+mysql_passwd="knicks"
+
